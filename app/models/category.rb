2a562392b3296aed28378bcc78c81cada7f56f9b
Marcos <marcosvto1@gmail.com>
2021-12-19 20:52:39 -0300

    add model product_categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
  
+  has_many :product_categories, dependent: :destroy
+  has_many :products, through: :product_categories
+
 end
