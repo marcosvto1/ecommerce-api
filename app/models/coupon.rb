9713cf32a200bb032b86ce2ac187d3db6bcd04ce
Marcos <marcosvto1@gmail.com>
2021-12-19 21:33:24 -0300

    configure coupon faker

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,7 +2,7 @@ class Coupon < ApplicationRecord
   validates :code, presence: true, uniqueness: { case_sensitive: false}
   validates :status, presence: true
   validates :due_date, presence: true
-  validates :discount_value, presence: true, numericality: { greater_than: 0 }
+  validates :discount_value, presence: true, numericality: {greater_than: 0 }
 
   enum status: { active: 1, inactive: 2 }
 end
--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :coupon do
-    code { "MyString" }
-    status { 1 }
-    decimal_value { "" }
+    code { Faker::Commerce.unique.promotion_code(digits: 6) }
+    status { %i(active inactive).sample }
+    decimal_value { rand(1..99) }
     due_date { "2021-12-19 21:05:20" }
   end
 end
