a338a78bb6f6e9c5b5d0d37fc652ebb012050e0e
Marcos <marcosvto1@gmail.com>
2021-12-26 20:17:25 -0300

    add shared concern models

--- spec/models/system_requirement_spec.rb
+++ spec/models/system_requirement_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe SystemRequirement, type: :model do
   it { is_expected.to validate_presence_of(:name) }
@@ -9,5 +9,8 @@ RSpec.describe SystemRequirement, type: :model do
   it { is_expected.to validate_presence_of(:memory) }
   it { is_expected.to validate_presence_of(:video_board) }
 
-  it { is_expected.to have_many(:games).dependent(:restrict_with_error)}
+  it { is_expected.to have_many(:games).dependent(:restrict_with_error) }
+
+  it_behaves_like "name searchable concern", :system_requirement
+  it_behaves_like "paginatable concern", :system_requirement
 end
