8fcc0142917dd4b1537aa72f7dfccd2d97a31352
Marcos <marcosvto1@gmail.com>
2021-12-27 23:14:59 -0300

    ensure returns errors when call with invalid params

--- app/controllers/admin/v1/system_requirements_controller.rb
+++ app/controllers/admin/v1/system_requirements_controller.rb
@@ -3,5 +3,23 @@ module Admin::V1
     def index
       @system_requirements = SystemRequirement.all
     end
+
+    def create
+      @system_requirement = SystemRequirement.new
+      @system_requirement.attributes = system_requirement_params
+      save_system_requirement!
+    end
+
+    private
+
+    def system_requirement_params
+      params.require(:system_requirement).permit(:name, :operational_system, :storage, :processor, :memory, :video_board)
+    end
+
+    def save_system_requirement!
+      @system_requirement.save!
+    rescue
+      render_error fields: @system_requirement.errors.messages
+    end
   end
 end
--- spec/requests/admin/v1/system_requirements/admin_request_spec.rb
+++ spec/requests/admin/v1/system_requirements/admin_request_spec.rb
@@ -17,4 +17,32 @@ RSpec.describe "Admin::V1::SystemRequirement as :admin", type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  context "POST /system_requirements" do
+    let(:url) { "/admin/v1/system_requirements" }
+    context "when invalid params" do
+      let(:invalid_params) do
+        { system_requirement: attributes_for(:system_requirement, name: nil, processor: nil) }.to_json
+      end
+      it "should does not save system_requirement" do
+        expect do
+          post url, headers: auth_header(user), params: invalid_params
+        end.to_not change(SystemRequirement, :count)
+      end
+
+      it "should return error" do
+        post url, headers: auth_header(user), params: invalid_params
+        expect(body_json["errors"]["fields"]).to have_key("name")
+        expect(body_json["errors"]["fields"]).to have_key("processor")
+      end
+
+      it "should return :unprocessable_entity status" do
+        post url, headers: auth_header(user), params: invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context "when valida params" do
+    end
+  end
 end
