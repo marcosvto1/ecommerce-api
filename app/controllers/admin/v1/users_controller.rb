12942e3499e723cf466c4ad09f1235ed445e05f9
Marcos <marcosvto1@gmail.com>
2022-01-02 20:45:10 -0300

    ensure update user with correct params

--- app/controllers/admin/v1/users_controller.rb
+++ app/controllers/admin/v1/users_controller.rb
@@ -1,5 +1,7 @@
 module Admin::V1
   class UsersController < ApiController
+    before_action :load_user, only: %i[update destroy]
+
     def index
       @users = User.all
     end
@@ -10,6 +12,11 @@ module Admin::V1
       save_user! :created
     end
 
+    def update
+      @user.attributes = user_params
+      save_user! :ok
+    end
+
     private
 
     def save_user!(status = :ok)
@@ -17,6 +24,10 @@ module Admin::V1
       render :show, status: status
     rescue
       render_error fields: @user.errors.messages
+    end
+
+    def load_user
+      @user = User.find(params[:id])
     end
 
     def user_params
--- spec/requests/admin/v1/users/admin_request_spec.rb
+++ spec/requests/admin/v1/users/admin_request_spec.rb
@@ -59,10 +59,67 @@ RSpec.describe "Admin::V1::User" do
         expect(body_json["errors"]["fields"]).to have_key("email")
       end
 
+      it "should returns error if password is not equal password_confirmation" do
+        user_with_password_is_difer = { user: attributes_for(:user, password: "123456789", password_confirmation: "987654331") }.to_json
+        post url, headers: auth_header(users.first), params: user_with_password_is_difer
+
+        expect(body_json["errors"]["fields"]).to have_key("password_confirmation")
+      end
+
+      it "should return error if email already in use" do
+        new_user_attr = User.new
+        new_user_attr.attributes = attributes_for(:user, email: "mail@mail.com")
+        new_user_attr.save
+
+        user_with_email_already_in_use = { user: attributes_for(:user, email: "mail@mail.com") }.to_json
+        post url, headers: auth_header(users.first), params: user_with_email_already_in_use
+
+        expect(body_json["errors"]["fields"]).to have_key("email")
+      end
+
       it "should return :unprocessable_entity status" do
         post url, headers: auth_header(users.first), params: incorrect_params
 
         expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  context "PATCH /users" do
+    let!(:user_saved) { create(:user, email: "teste@teste.com") }
+    let(:url) { "/admin/v1/users/#{user_saved.id}" }
+
+    context "when correct params" do
+      let(:correct_params) { { user: attributes_for(:user, email: "teste2@teste.com") }.to_json }
+      it "should update a user" do
+        patch url, headers: auth_header(users.last), params: correct_params
+
+        user_saved.reload
+
+        expect(user_saved.email).to eq "teste2@teste.com"
+      end
+
+      it "should returns user updated" do
+        patch url, headers: auth_header(users.last), params: correct_params
+        user_saved.reload
+        expected_user = user_saved.as_json only: %i(id name email profile)
+        expect(body_json["user"]).to eq expected_user
+      end
+
+      it "should return status :ok" do
+        patch url, headers: auth_header(users.last), params: correct_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "when incorrect params" do
+      let(:incorect_params) { { user: attributes_for(:user, email: nil) }.to_json }
+
+      it "should not update" do
+        patch url, headers: auth_header(users.last), params: incorect_params
+
+        user_saved.reload
+        expect(user_saved.email).to eq "teste@teste.com"
       end
     end
   end
